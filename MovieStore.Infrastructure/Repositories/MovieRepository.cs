1ac5c3a7b2abd6184b247a278bb50de34b9acdc7
Yichen Wang <wangyichen3@gmail.com>
2020-07-16 20:30:38 -0700

    modify get25highestRatingMovie

--- MovieStore.Infrastructure/Repositories/MovieRepository.cs
+++ MovieStore.Infrastructure/Repositories/MovieRepository.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query.SqlExpressions;
 using MovieStore.Core.Entities;
 using MovieStore.Core.RepositoryInterfaces;
 using MovieStore.Infrastructure.Data;
@@ -18,9 +19,16 @@ namespace MovieStore.Infrastructure.Repositories
 
         public async Task<IEnumerable<Movie>> GetHighestRatedMovies()
         {
-            var movie = await _dbContext.Movies.OrderByDescending(m => m.Rating).Take(25).ToListAsync();
-            return movie;
+            //var movie = await _dbContext.Movies.OrderByDescending(m => m.Rating).Take(25).ToListAsync();
+            //return movie;
+            var movies = await _dbContext.Reviews
+                        .Join(_dbContext.Movies,
+                        r => r.MovieId,
+                        m => m.Id,
+                        (r, m) => new { r, m }).GroupBy(x => x.r.MovieId).OrderByDescending(x=>x.Key).Take(25).ToListAsync();
+            return (IEnumerable<Movie>)movies;
         }
+
 
         public async Task<IEnumerable<Movie>> GetHighestRevenueMovies()
         {
